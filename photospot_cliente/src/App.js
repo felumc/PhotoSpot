e0b5d90085f149f6345c51bbcd1b4d4b8a3b0485
Luis Morales <luismc2211mail.com>
2022-11-07 16:24:32 -0600

    Login, Register y Contacto

--- photospot_cliente/src/App.js
+++ photospot_cliente/src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+//import LoginUsers from './pages/LoginUsers';
+//import RegisterUsers from './pages/RegisterUsers';
+import Contacto from './pages/Contacto';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Contacto /> 
+      
     </div>
   );
 }
